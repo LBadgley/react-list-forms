c05e2cc6d559fc92090d264c78399aac3355c2eb
Laura Badgley <l.badgleyy@gmail.com>
2019-05-15 16:25:49 -0700

    rendering text but no fun font options

--- src/components/TextFormatter.js
+++ src/components/TextFormatter.js
@@ -1,23 +1,39 @@
 import React, { PureComponent } from 'react';
+import figlet from 'figlet';
 
 export default class textFormatter extends PureComponent {
   state = {
     text: '',
-    color: 'black',
+    formattedText: '',
+    color: '#000000',
+    font: 'Big'
   };
 
+  // formatText = () => {
+  //   const { text, font } = this.state;
+  //   figlet.text(text, {
+  //     font: font
+  //   }, (err, result) => {
+  //     this.setState({ formattedText: result });
+  //   });
+  // }
   changeHandler = ({ target }) => {
     this.setState({ [target.name]: target.value });
   }
-
+  
   render() {
-    const { text, color } = this.state;
+    const { text, color, } = this.state;
+
+    // const fontOptions = ['big', 'bubble', 'cosmic', 'isometric2'].map(font => {
+    //   return <option key={font} value={font}>{font}</option>;
+    // });
+
     return (
-      <form>
-        <input type="text" name="text" value={text} onChange={this.changeHandler} />
-        <input type="color" name="color" value={color} onChange={this.changeHandler} />
+      <>
+        <input name="text" value={text} onChange={this.changeHandler} />
+        <input name="color" type="color" value={color} onChange={this.changeHandler} />
         <p style={{ color: color }}>{text}</p>
-      </form>
+      </>
     );
   }
 }
