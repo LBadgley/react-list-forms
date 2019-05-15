0ca7b1d782a93788d81211646778d91a8f431cbd
Laura Badgley <l.badgleyy@gmail.com>
2019-05-15 15:12:18 -0700

    renders app test passing

---
+++ src/components/TextFormatter.js
@@ -0,0 +1,23 @@
+import React, { PureComponent } from 'react';
+
+export default class textFormatter extends PureComponent {
+  state = {
+    text: '',
+    color: 'black',
+  };
+
+  changeHandler = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+  render() {
+    const { text, color } = this.state;
+    return (
+      <form>
+        <input type="text" name="text" value={text} onChange={this.changeHandler} />
+        <input type="color" name="color" value={color} onChange={this.changeHandler} />
+        <p style={{ color: color }}>{text}</p>
+      </form>
+    );
+  }
+}
