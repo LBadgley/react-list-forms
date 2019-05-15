2fa5b95bf57c930f2131e5b55d7488858b6a0726
Laura Badgley <l.badgleyy@gmail.com>
2019-05-15 14:42:15 -0700

    testing photo component

--- src/components/photos/Photo.test.js
+++ src/components/photos/Photo.test.js
@@ -2,4 +2,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Photo from './Photo';
 
-
+describe('photo component', () => {
+  it('renders a Photo', () => {
+    const wrapper = shallow(<Photo photo="/path/to/photo" />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
