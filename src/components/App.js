eaf62f3072f341f5f69667e48ce2e21814319e1c
Laura Badgley <l.badgleyy@gmail.com>
2019-05-17 10:25:56 -0700

    styled my photo album

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ export default function App() {
   return (
     <>
       <Header />
-      <PhotoAlbum title="Cats" photos={photos} />
+      <PhotoAlbum title="Kids" photos={photos} />
       <Footer />
     </>
   );
