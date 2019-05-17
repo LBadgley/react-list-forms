1b00288c4d43e81df475528c0faaf1b10749b2db
Laura Badgley <l.badgleyy@gmail.com>
2019-05-17 11:12:39 -0700

    fixed photos.test

--- src/components/photos/Photos.test.js
+++ src/components/photos/Photos.test.js
@@ -3,8 +3,13 @@ import { shallow } from 'enzyme';
 import Photos from './Photos';
 
 describe('photos test', () => {
-  it('renders the photos', () =>{
-    const wrapper = shallow(<Photos />);
+  it('renders the photos', () => {
+    const photos = [
+      '/img1',
+      '/img2',
+      '/img3'
+    ];
+    const wrapper = shallow(<Photos photos={photos} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
